b1bba47bd771dd6db387840241c63a2093c88769
Brancen Gregory <brancengregory@gmail.com>
2025-07-19 15:21:52 -0500

    Red/green led example

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
-fn main() {
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::*;
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_sys as _;
+use log::info;
+
+fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
-
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    info!("Starting Red/Green LED Flash");
+
+    let peripherals = Peripherals::take().unwrap();
+
+    let mut led_red = PinDriver::output(peripherals.pins.gpio2)?;
+    let mut led_green = PinDriver::output(peripherals.pins.gpio3)?;
+
+    led_red.set_low()?;
+    led_green.set_low()?;
+
+    loop {
+        info!("Turning Red LED ON");
+        led_red.set_high()?;
+        led_green.set_low()?;
+        FreeRtos::delay_ms(1000);
+
+        info!("Turning Green LED ON");
+        led_red.set_low()?;
+        led_green.set_high()?;
+        FreeRtos::delay_ms(1000);
+    }
 }
