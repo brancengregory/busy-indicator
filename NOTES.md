22c1292eb224adfc118b38b61c6b234fea46c184
Brancen Gregory <brancengregory@gmail.com>
2025-07-20 13:23:00 -0500

    Add enclosure notes; update program to two color blink

---
+++ NOTES.md
@@ -0,0 +1,22 @@
+## Configuration
+
+- Stacked battery + ESP
+- USB port on 34mm enclosure width, with USB extending through wall
+- LEDs exposed on top surface via small hole that led doesn't pass through but bubbles through
+
+## Dimensions
+
+### Enclosure
+
+- 60mm x 34mm x 18mm
+- 2mm wall thickness
+
+### XIAO ESP32C3
+
+- 21mm x 17.5mm x 4.5mm
+- 1.6mm USB overhang
+
+### Battery
+
+- 49mm x 30mm x 8mm
+
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,32 @@
-use esp_idf_hal::delay::{Delay, Ets, FreeRtos};
-use esp_idf_hal::gpio::{Output, Pin, PinDriver};
+use esp_idf_hal::delay::Delay;
+use esp_idf_hal::gpio::PinDriver;
 use esp_idf_hal::peripherals::Peripherals;
-use esp_idf_hal::prelude::*;
-use embedded_hal::delay::DelayNs;
-use log::{info};
+use log::info;
 
 fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    info!("Starting LED test on XIAO ESP32C3");
+    info!("Starting LEDs on XIAO ESP32C3");
 
     let peripherals = Peripherals::take().unwrap();
-    let mut led = PinDriver::output(peripherals.pins.gpio2)?;
+    let mut red_led = PinDriver::output(peripherals.pins.gpio3)?;
+    let mut green_led = PinDriver::output(peripherals.pins.gpio4)?;
 
-    let mut delay = Delay::new_default();
+    let delay = Delay::new_default();
 
-    led.set_low()?;
+    red_led.set_low()?;
+    green_led.set_low()?;
 
     loop {
-        info!("Turning LED ON");
-        led.set_high()?;
+        info!("Turning red LED ON");
+        green_led.set_low()?;
+        red_led.set_high()?;
         delay.delay_ms(1000);
-        info!("Turning LED OFF");
-        led.set_low()?;
+
+        info!("Turning green LED ON");
+        red_led.set_low()?;
+        green_led.set_high()?;
         delay.delay_ms(1000);
     }
 }
