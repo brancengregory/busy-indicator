3f461db057e475d88f125b487214b099f2d19698
Brancen Gregory <brancengregory@gmail.com>
2025-07-19 22:53:00 -0500

    Simplified program

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,29 @@
-use esp_idf_hal::delay::FreeRtos;
-use esp_idf_hal::gpio::*;
+use esp_idf_hal::delay::{Delay, Ets, FreeRtos};
+use esp_idf_hal::gpio::{Output, Pin, PinDriver};
 use esp_idf_hal::peripherals::Peripherals;
-use esp_idf_sys as _;
-use log::info;
+use esp_idf_hal::prelude::*;
+use embedded_hal::delay::DelayNs;
+use log::{info};
 
 fn main() -> anyhow::Result<()> {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
-    // Bind the log crate to the ESP Logging facilities
+    esp_idf_sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    info!("Starting Red/Green LED Flash");
+    info!("Starting LED test on XIAO ESP32C3");
 
     let peripherals = Peripherals::take().unwrap();
+    let mut led = PinDriver::output(peripherals.pins.gpio2)?;
 
-    let mut led_red = PinDriver::output(peripherals.pins.gpio2)?;
-    let mut led_green = PinDriver::output(peripherals.pins.gpio3)?;
+    let mut delay = Delay::new_default();
 
-    led_red.set_low()?;
-    led_green.set_low()?;
+    led.set_low()?;
 
     loop {
-        info!("Turning Red LED ON");
-        led_red.set_high()?;
-        led_green.set_low()?;
-        FreeRtos::delay_ms(1000);
-
-        info!("Turning Green LED ON");
-        led_red.set_low()?;
-        led_green.set_high()?;
-        FreeRtos::delay_ms(1000);
+        info!("Turning LED ON");
+        led.set_high()?;
+        delay.delay_ms(1000);
+        info!("Turning LED OFF");
+        led.set_low()?;
+        delay.delay_ms(1000);
     }
 }
